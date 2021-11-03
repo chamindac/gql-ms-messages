0a117007621e65f6ce501bc505e0c1de729be3fb
Chaiminda Chandrasekara <chaminda@ekriegers.com>
2021-11-03 18:05:53 +0530

    order created msg

---
+++ src/messages/OrderCreated.cs
@@ -0,0 +1,13 @@
+﻿using EasyNetQ;
+using System;
+
+namespace messages
+{
+    [Queue("OrderCreatedQueue",ExchangeName = "OrderCreatedExchange")]
+    public class OrderCreatedMessage
+    {
+        public int OrderId { get; set; }
+        public string OrderNumber { get; set; }
+        public decimal OrderAmount { get; set; }
+    }
+}
