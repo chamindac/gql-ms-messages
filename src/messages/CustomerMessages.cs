4b011336aa0e01863f4166a2c614e4a0d111a3a6
Chaiminda Chandrasekara <chaminda@ekriegers.com>
2021-11-04 13:11:45 +0530

    New messages

---
+++ src/messages/CustomerMessages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace messages
+{
+    public class CustomerOrderCreditAvailableMessage
+    {
+        public int OrderId { get; set; }
+    }
+
+    public class CustomerOrderCreditNotAvailableMessage
+    {
+        public int OrderId { get; set; }
+    }
+}
--- src/messages/OrderMessages.cs
+++ src/messages/OrderMessages.cs
@@ -3,10 +3,11 @@ using System;
 
 namespace messages
 {
-    [Queue("OrderCreatedQueue",ExchangeName = "OrderCreatedExchange")]
+    //[Queue("OrderCreatedQueue",ExchangeName = "OrderCreatedExchange")]
     public class OrderCreatedMessage
     {
         public int OrderId { get; set; }
+        public string CustomerCode { get; set; }
         public string OrderNumber { get; set; }
         public decimal OrderAmount { get; set; }
     }
